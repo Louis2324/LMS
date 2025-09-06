4bfb286228f469bea5820780a21fb70668e7f698
Louis2324 <miguellouis2023@gmail.com>
2025-09-06 22:05:01 +0200

    Integrate database connection and environment variables management

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+
 const app = express();
+dotenv.config();
 
-
-const server = app.listen(5000,()=>{
-    console.log("Server is up and running");
+app.listen(process.env.PORT || 4000,()=>{
+    console.log(`Server is up and running at port http://localhost:${process.env.PORT || 4000}`);
+    connectDB();
 })
