4fcac6bf4c509df909142a058855510985fa37af
Louis2324 <miguellouis2023@gmail.com>
2025-09-07 12:19:10 +0200

    Added user routes to the api

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 dotenv.config();
+app.use(express.json());
+
+app.use("/api/user", userRouter);
 
 app.listen(process.env.PORT || 4000,()=>{
-    console.log(`Server is up and running at port http://localhost:${process.env.PORT || 4000}`);
+    console.log(`Server is up and running at http://localhost:${process.env.PORT || 4000}`);
     connectDB();
 })
