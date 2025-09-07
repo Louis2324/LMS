2f2d01fdd57a373431eb20aec31007df8edc1c39
Louis2324 <miguellouis2023@gmail.com>
2025-09-07 07:18:21 +0200

    added password encryption and comparison for role authentication

--- models/User.js
+++ models/User.js
@@ -1,9 +1,23 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     name : {type:String, required:true },
-    email: {type:String, required:true},
+    email: {type:String, unique:true,required:true},
+    password:{type:String,required:true},
+    role: {type:String,enum:["user","admin"], default:"user"},
+    status:{type:String,enum:["active","suspended"],default:"active"}
 },{timestamps:true});
+
+userSchema.pre("save", async ()=>{
+ if(!this.isModified("password")) return;
+ const salt = await bcrypt.genSalt(10);
+ this.password = await bcrypt.hash(this.password,salt);
+});
+
+userSchema.methods.comparePassword = async (enteredPassword) => {
+    return await bcrypt.compare(enteredPassword , this.password);
+}
 
 const User = mongoose.model("user",userSchema);
 export default User;
