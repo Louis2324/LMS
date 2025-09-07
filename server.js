c564ba3df9ff3bdec8888a888c460bd4f03d129e
Louis2324 <miguellouis2023@gmail.com>
2025-09-07 18:52:28 +0200

    Added book routes to the api

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
+import bookRouter from "./routes/bookRoutes.js";
 
 const app = express();
 dotenv.config();
 app.use(express.json());
 
 app.use("/api/user", userRouter);
+app.use("/api/books",bookRouter);
 
 app.listen(process.env.PORT || 4000,()=>{
     console.log(`Server is up and running at http://localhost:${process.env.PORT || 4000}`);
