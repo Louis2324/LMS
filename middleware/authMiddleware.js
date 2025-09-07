4ab37e809262404a398c9660294c1f674eda2efa
Louis2324 <miguellouis2023@gmail.com>
2025-09-07 22:22:35 +0200

    added middleware for role based authentication

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+export const authenticateUser = (req,res,next) => {
+ const authHeader = req.headers.authorization;
+ if(!authHeader || authHeader.startsWith("Bearer ")) return res.json(401).json({msg:"Unauthorized"});
+ try {
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.decode(token,process.env.JWT_SECRET);
+    req.user = {id: decoded.userId , role:decoded.role}
+    next();
+ } catch (error) {
+    return res.status(401).json({msg:"Bad or Invalid token"});
+ }
+};
+
+export const authorizeRoles = (...allowedRoles) => {
+    return (req,res,next) => {
+        if(!allowedRoles.includes(req.user.role)){
+            return res.status(403).json({msg:"Access denied"});
+        }
+        next();
+    }
+}
