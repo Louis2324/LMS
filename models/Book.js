01a0d3e5eeacb538103383695813d6662ac8cd29
Louis2324 <miguellouis2023@gmail.com>
2025-09-07 18:51:46 +0200

    Fitted the model to the system's needs

--- models/Book.js
+++ models/Book.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
+
 const bookSchema = mongoose.Schema({
     title:{type:String,required:true},
     author:{type:String,required:true},
+    isbn: {type:String , required:true, sparse:true},
     category:{type:String,required:true},
-})
+    copies: {type:Number, required:true,min:1},
+    availableCopies:{type:Number, default: function () { return this.copies}},
+    borrowedBy:{type:mongoose.Schema.types.id, ref:"User",default:null},
+    dueDate:{type:Date,default:null},
+},{timestamps:true});
 
 const Book = mongoose.model('Book',bookSchema)
 export default Book;
