ef6667acc84a2a446b830d9128e989e0bfa09a88
Louis2324 <miguellouis2023@gmail.com>
2025-09-08 21:10:38 +0200

    Added automation for the disabling users feature

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
 import bookRouter from "./routes/bookRoutes.js";
 import { errorHandler,notFound } from "./middleware/errorHandler.js";
+import { disableOverDueUsers } from "./controllers/userController.js";
 
 const app = express();
 dotenv.config();
@@ -14,6 +15,12 @@ app.use("/api/books",bookRouter);
 
 app.use(notFound);
 app.use(errorHandler);
+
+setInterval(()=>{
+    disableOverDueUsers()
+     .then(()=>console.log("Checked for overdue users"))
+     .catch(err => console.error("Error disabling overdue users:", err));
+});
 app.listen(process.env.PORT || 4000,()=>{
     console.log(`Server is up and running at http://localhost:${process.env.PORT || 4000}`);
     connectDB();
