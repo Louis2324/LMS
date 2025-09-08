fc6c9c548bea76555014c077be99bbabe08d046d
Louis2324 <miguellouis2023@gmail.com>
2025-09-08 21:07:19 +0200

    Added controller for disabling users who are past their grace periods

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import Loan from "../models/Loan.js";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
@@ -10,7 +11,7 @@ const createJWT = (user) => {
 
 export const registerUser = async (req,res,next) =>{
   try {
-      const {name,password,email,role} = req.body;
+    const {name,password,email,role} = req.body;
      const existingUser = await User.findOne({email});
      if(existingUser) {
         return res.status(400).json({msg:"Email already in use"});
@@ -33,21 +34,48 @@ export const registerUser = async (req,res,next) =>{
 
 export const login = async (req,res,next) => {
   try {
-        const {email , password} = req.body;
-        const user = await User.findOne({email});
-        if(!user || !(await user.comparePassword(password))) {
-            return res.status(401).json({msg:"Invalid Credentials"});
-        }
-        const token = createJWT(user);
-        return res.status(200).json({
-            user: {
-                _id:user._id,
-                name:user.name,
-                email:user.email,
-                role:user.role,
-            },
-            token});
+    const {email , password} = req.body;
+    const user = await User.findOne({email});
+    if(!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({msg:"Invalid Credentials"});
+    }
+    const token = createJWT(user);
+    return res.status(200).json({
+        user: {
+         _id:user._id,
+         name:user.name,
+         email:user.email,
+          role:user.role,
+        },
+      token});
   } catch (error) {
     next(error);
   }
+}
+
+export const disableOverDueUsers = async (req,res,next) => {
+ try {
+    const now = new Date();
+    const overdueLoans = Loan.find({
+      returnDate:null,
+      dueDate: {$lt: new Date(now.getTime() - 24*60*60*1000) },
+    }).populate("user");
+
+    if(overdueLoans.length === 0) return res.status(200).json({msg:"No overdue users to disable "});
+    const disabledUsers = [];
+    for(const loan of overdueLoans) {
+      const user = loan.user;
+      if (user.status !== "disabled"){
+        user.status = "disabled";
+        await user.save();
+        disabledUsers.push({_id:user._id , name:user.name , email: user.email});
+      }
+    }
+    return res.status(200).json({
+      msg:"Overdue users disabled succesfully",
+      disabledUsers,
+    });
+ } catch (error) {
+  
+ }
 }
