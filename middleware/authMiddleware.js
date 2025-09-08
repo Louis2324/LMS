eead133c1dfd4d1e25a67afc6aa995b17f3610d6
Louis2324 <miguellouis2023@gmail.com>
2025-09-08 14:13:31 +0200

    Fixed the checking condition and switched to json.verify to test if the token is still time valid

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,10 +1,10 @@
 import jwt from "jsonwebtoken";
 export const authenticateUser = (req,res,next) => {
  const authHeader = req.headers.authorization;
- if(!authHeader || authHeader.startsWith("Bearer ")) return res.json(401).json({msg:"Unauthorized"});
+ if(!authHeader || !authHeader.startsWith("Bearer ")) return res.status(401).json({msg:"Unauthorized"});
  try {
     const token = authHeader.split(" ")[1];
-    const decoded = jwt.decode(token,process.env.JWT_SECRET);
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
     req.user = {id: decoded.userId , role:decoded.role}
     next();
  } catch (error) {
