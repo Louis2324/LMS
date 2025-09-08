42bdc699590c5746f729828c2c14be06e7c55138
Louis2324 <miguellouis2023@gmail.com>
2025-09-08 09:40:22 +0200

    Added errorhandling middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
 import bookRouter from "./routes/bookRoutes.js";
+import { errorHandler,notFound } from "./middleware/errorHandler.js";
 
 const app = express();
 dotenv.config();
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/books",bookRouter);
 
+app.use(notFound);
+app.use(errorHandler);
 app.listen(process.env.PORT || 4000,()=>{
     console.log(`Server is up and running at http://localhost:${process.env.PORT || 4000}`);
     connectDB();
