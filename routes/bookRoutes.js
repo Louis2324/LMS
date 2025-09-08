c29cee0f862edcd76377f6c2c11cdd304e95fa2e
Louis2324 <miguellouis2023@gmail.com>
2025-09-08 14:14:54 +0200

    Added loaning system routes and added authentication middleware for admin only access features and in system users

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,12 +2,17 @@ import express from "express";
 import { getBook,getBooks,addBook,updateBook,deleteBook } from "../controllers/bookController.js";
 import { validate } from "../middleware/validator.js";
 import { bookValidatorSchema,updateValidatorSchema } from "../validators/bookValidator.js";
+import { borrowBook ,returnBook } from "../controllers/loanController.js";
+import { authenticateUser , authorizeRoles} from "../middleware/authMiddleware.js";
 const router = express.Router();
 
-router.get("/",getBooks);
-router.get("/:id",getBook);
-router.post("/",validate(bookValidatorSchema),addBook);
-router.put("/:id",validate(updateValidatorSchema),updateBook);
-router.delete("/:id",deleteBook);
+router.get("/",authenticateUser,getBooks);
+router.get("/:id",authenticateUser,getBook);
+router.post("/",authenticateUser,authorizeRoles("admin"),validate(bookValidatorSchema),addBook);
+router.put("/:id",authenticateUser,authorizeRoles("admin"),validate(updateValidatorSchema),updateBook);
+router.delete("/:id",authenticateUser,authorizeRoles("admin"),deleteBook);
+
+router.put("/:id/borrow",authenticateUser,borrowBook);
+router.put("/:id/return",authenticateUser,returnBook);
 
 export default router;
