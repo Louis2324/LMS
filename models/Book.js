6bdd81d79be0e4c9f12777892c39bf896d805133
Louis2324 <miguellouis2023@gmail.com>
2025-09-08 15:21:13 +0200

    Removed the loaning system related fields

--- models/Book.js
+++ models/Book.js
@@ -6,9 +6,6 @@ const bookSchema = mongoose.Schema({
     isbn: {type:String , required:true, sparse:true},
     category:{type:String,required:true},
     copies: {type:Number, required:true,min:1},
-    availableCopies:{type:Number, default: function () { return this.copies}},
-    borrowedBy:{type:mongoose.Schema.Types.ObjectId, ref:"user",default:null},
-    dueDate:{type:Date,default:null},
 },{timestamps:true});
 
 const Book = mongoose.model('Book',bookSchema)
