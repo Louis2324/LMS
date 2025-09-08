bae053ba38fc2482880e0717953f235c0cbed85a
Louis2324 <miguellouis2023@gmail.com>
2025-09-08 16:33:45 +0200

    Added the endpoint for the loaned books

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,11 +2,12 @@ import express from "express";
 import { getBook,getBooks,addBook,updateBook,deleteBook } from "../controllers/bookController.js";
 import { validate } from "../middleware/validator.js";
 import { bookValidatorSchema,updateValidatorSchema } from "../validators/bookValidator.js";
-import { borrowBook ,returnBook } from "../controllers/loanController.js";
+import { borrowBook ,getLoans,returnBook } from "../controllers/loanController.js";
 import { authenticateUser , authorizeRoles} from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.get("/",authenticateUser,getBooks);
+router.get("/loans",authenticateUser,getLoans);
 router.get("/:id",authenticateUser,getBook);
 router.post("/",authenticateUser,authorizeRoles("admin"),validate(bookValidatorSchema),addBook);
 router.put("/:id",authenticateUser,authorizeRoles("admin"),validate(updateValidatorSchema),updateBook);
@@ -14,5 +15,6 @@ router.delete("/:id",authenticateUser,authorizeRoles("admin"),deleteBook);
 
 router.put("/borrow/:id",authenticateUser,borrowBook);
 router.put("/return/:id",authenticateUser,returnBook);
+
 
 export default router;
