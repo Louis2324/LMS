985a436f5a4ba854e63f09340cc90fab57cf3154
Louis2324 <miguellouis2023@gmail.com>
2025-09-08 16:16:43 +0200

    Fixed mismatching model fields and controllers

--- controllers/loanController.js
+++ controllers/loanController.js
@@ -7,11 +7,11 @@ export const borrowBook = async (req,res,next) => {
         const bookId = req.params.id;
         const userId = req.user.id;
 
-        const activeLoans = Loan.countDocuments({user: userId , returnedAt:null });
+        const activeLoans = Loan.countDocuments({user: userId , returnDate:null });
         if(activeLoans>=3) return res.status(403).json({msg:"You cannot borrow more than 3 books"});
         const book = await Book.findById(bookId);
         if(!book) return res.status(404).json({msg:"Book not found"});
-        const totalLoansForBook = Loan.countDocuments({book:bookId,returnedAt: null});
+        const totalLoansForBook = Loan.countDocuments({book:bookId,returnDate: null});
         if(totalLoansForBook >= book.copies) return res.status(409).json({msg:"No available copies for borrowing"});
     
         const due = new Date();
@@ -20,6 +20,7 @@ export const borrowBook = async (req,res,next) => {
         const loan = await Loan.create({
             user:userId,
             book:bookId,
+            borrowedAt: Date.now(),
             dueDate:due,
         })
         return res.status(200).json({msg:"Borrowed Book Succesfully"},loan);
@@ -32,13 +33,11 @@ export const returnBook = async (req,res,next) => {
  try {
     const bookId = req.params.id;
     const userId = req.user.id;
-
-    const loan = await Loan.findOne({book:bookId,user:userId,returnedAt:null});
-    if(!loan) res.status(403).json({msg:"Can not return a book you didn't borrow"});
-    loan.returnedAt = new Date();
+    const loan = await Loan.findOne({book:bookId,user:userId,returnDate:null});
+    if(!loan) return res.status(403).json({msg:"Can not return a book you didn't borrow"});
+    loan.returnDate = new Date();
     await loan.save();
-    return res.status(200).json({msg:"Book returned successfully"},loan);
-    
+    return res.status(200).json({msg:"Book returned successfully",loan});
  } catch (error) {
     next(error);
  }
