7ec52497f508dec8d020f91eb606514411946610
Louis2324 <miguellouis2023@gmail.com>
2025-09-08 06:19:07 +0200

    Added the validation middleware to validate the routes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { getBook,getBooks,addBook,updateBook,deleteBook } from "../controllers/bookController.js";
+import { validate } from "../middleware/validator.js";
+import { bookValidatorSchema,updateValidatorSchema } from "../validators/bookValidator.js";
 const router = express.Router();
 
 router.get("/",getBooks);
 router.get("/:id",getBook);
-router.post("/",addBook);
-router.put("/:id",updateBook);
+router.post("/",validate(bookValidatorSchema),addBook);
+router.put("/:id",validate(updateValidatorSchema),updateBook);
 router.delete("/:id",deleteBook);
 
 export default router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { registerUser , login } from "../controllers/userController.js";
-
+import { validate } from "../middleware/validator.js";
+import { userValidatorSchema,loginValidatorSchema } from "../validators/userValidators.js";
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login",login);
+router.post("/register",validate(userValidatorSchema), registerUser);
+router.post("/login",validate(loginValidatorSchema),login);
 
 export default router;
